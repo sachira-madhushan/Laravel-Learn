6ae52eb2e0156da3a094e117da8252db8510a911
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-03-27 16:39:29 +0530

    Layouts & Slots

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Layout</title>
+</head>
+<body>
+    <h1>This is the logo</h1>
+    <main>
+        {{$slot}}
+    </main>
+</body>
+</html>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,11 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
+<x-layout>
     <h1>Home</h1>
     <p>{{$greeting}}</p>
 
@@ -17,5 +10,5 @@
         <p>If condition is true</p>
     @endif
     <a href="/welcome">Click here</a>
-</body>
-</html>
+
+</x-layout>
