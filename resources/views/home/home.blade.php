bc9ce1dd7ed50c09af6b9038e0fd65c8946d1c16
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-03-27 16:08:09 +0530

    Blade Directives

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -8,8 +8,14 @@
 <body>
     <h1>Home</h1>
     <p>{{$greeting}}</p>
-    <p>{{$heros[0]["name"]}}</p>
-    <p>{{$heros[1]["name"]}}</p>
+
+    @foreach ($heros as $hero)
+            <p>{{$hero['name']." ".$hero['skill']}}</p>
+    @endforeach
+
+    @if ($greeting=="hello")
+        <p>If condition is true</p>
+    @endif
     <a href="/welcome">Click here</a>
 </body>
 </html>
