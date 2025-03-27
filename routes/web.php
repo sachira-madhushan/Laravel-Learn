b7e72f859647bd2799324e8768c08a0bbb4409ef
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-03-27 15:29:50 +0530

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get("/home",function(){
+    return view('home.home');
 });
