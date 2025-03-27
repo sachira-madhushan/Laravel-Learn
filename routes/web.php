d1be05e785d26f165e198c6a8b90ef1ad1b5aa75
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-03-27 15:49:23 +0530

    wildcards

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,13 @@ Route::get("/home",function(){
 
     return view('home.home',["greeting"=>"hello","heros"=>$heros]);
 });
+
+Route::get("/hero/{id}",function($id){
+
+    $heros=[
+        ["name"=>"mario","skill"=>75],
+        ["name"=>"ben10","skill"=>80]
+    ];
+
+    return view('home.show',["heros"=>$heros,"id"=>$id]);
+});
