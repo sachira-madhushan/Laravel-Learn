f073d9e41faf0cd98663ee57e2af58dd0b19f2ea
sachira-madhushan <sacheeramadushan455@gmail.com>
2025-03-28 10:21:03 +0530

    Eloquent Models

--- database/migrations/2025_03_28_043705_create_ninjas_table.php
+++ database/migrations/2025_03_28_043705_create_ninjas_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('todos', function (Blueprint $table) {
+        Schema::create('ninjas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->integer('skill');
+            $table->string('bio');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('todos');
+        Schema::dropIfExists('ninjas');
     }
 };
